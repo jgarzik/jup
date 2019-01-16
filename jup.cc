687e7fcedbef18b692e6f9128a9f0d9f0be4c9ac
Jeff Garzik <jeff@bloq.com>
2019-01-16 10:12:29 -0500

    jup: implement 'get' command

--- jup.cc
+++ jup.cc
@@ -1,11 +1,13 @@
 
 #include "jup-config.h"
 #include <vector>
+#include <deque>
 #include <string>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <ctype.h>
 #include <argp.h>
 #include <univalue.h>
 
@@ -22,7 +24,23 @@ static const char args_doc[] =
 
 static struct argp_option options[] = {
 	{"min", 1001, 0, 0, "Minimize JSON output"},
+	{"list-commands", 1002, 0, 0, "List all supported edit commands"},
+
 	{ }
+};
+
+class commandInfo {
+public:
+	unsigned int n_args;
+	string name;
+	string summary;
+	string description;
+	bool ignoreStdin;
+};
+
+static commandInfo commandList[] = {
+	{ 1, "get", "get JSON-PATH",
+	  "Replace document with subset of JSON input, starting at JSON-PATH" },
 };
 
 static error_t parse_opt (int key, char *arg, struct argp_state *state);
@@ -30,13 +48,55 @@ static const struct argp argp = { options, parse_opt, args_doc, doc };
 
 static bool readStdin = true;
 static bool minimalJson = false;
+static bool doListCommands = false;
 UniValue jdoc(UniValue::VNULL);
+map<string,commandInfo> cmdMap;
+deque<string> inputTokens;
+
+#ifndef ARRAY_SIZE
+#define ARRAY_SIZE(x) (sizeof(x)/sizeof((x)[0]))
+#endif
+
+static void staticInit()
+{
+	for (unsigned int i = 0; i < ARRAY_SIZE(commandList); i++) {
+		cmdMap[commandList[i].name] = commandList[i];
+	}
+}
+
+static void listCommands()
+{
+	UniValue l(UniValue::VARR);
+
+	for (map<string,commandInfo>::const_iterator it = cmdMap.begin();
+	     it != cmdMap.end(); it++) {
+		UniValue obj(UniValue::VOBJ);
+
+		const commandInfo& ci = it->second;
+
+		obj.pushKV("command", ci.name);
+		obj.pushKV("usage", ci.summary);
+		obj.pushKV("help", ci.description);
+
+		l.push_back(obj);
+	}
+
+	printf("%s\n", l.write(minimalJson ? 0 : 2).c_str());
+}
 
 static error_t parse_opt (int key, char *arg, struct argp_state *state)
 {
 	switch (key) {
 	case 1001:
 		minimalJson = true;
+		break;
+
+	case 1002:
+		doListCommands = true;
+		break;
+
+	case ARGP_KEY_ARG:
+		inputTokens.push_back(arg);
 		break;
 
 	case ARGP_KEY_END:
@@ -88,6 +148,90 @@ static bool writeStringFd(int fd, const string& rawBody)
 	return true;
 }
 
+static void strsplit(const std::string& s_in, const std::string& delim,
+		     std::deque<std::string>& v)
+{
+	char s[s_in.size() + 1];
+	strcpy(s, s_in.c_str());
+
+	char *p = strtok(s, delim.c_str());
+	while (p != NULL) {
+		v.push_back(p);
+		p = strtok(NULL, delim.c_str());
+	}
+}
+
+static bool isDigitStr(const string& s)
+{
+	for (unsigned int i = 0; i < s.size(); i++)
+		if (!isdigit(s[i]))
+			return false;
+
+	return true;
+}
+
+static const UniValue& lookupPath(const string& path, deque<string> rem)
+{
+	rem.clear();
+
+	deque<string> tokens;
+
+	strsplit(path, ".", tokens);
+
+	UniValue& jptr = jdoc;
+	while (tokens.size() > 0) {
+		const string& token = tokens.front();
+
+		if (jptr.isObject() && jptr.exists(token)) {
+			// direct path match
+			if (tokens.size() == 1)
+				return jptr[token];
+
+			if (!jptr[token].isObject() &&
+			    !jptr[token].isArray()) {
+				rem = tokens;
+				return jptr;
+			}
+
+			jptr = jptr[token];
+			tokens.pop_front();
+
+		} else if (jptr.isArray() && isDigitStr(token) &&
+			   (atol(token.c_str()) < jptr.size())) {
+
+			size_t index = atol(token.c_str());
+			// direct path match
+			if (tokens.size() == 1)
+				return jptr[index];
+
+			if (!jptr[index].isObject() &&
+			    !jptr[index].isArray()) {
+				rem = tokens;
+				return jptr;
+			}
+
+			jptr = jptr[index];
+			tokens.pop_front();
+		} else {
+			rem = tokens;
+			return jptr;
+		}
+	}
+
+	return jptr;
+}
+
+const UniValue& objectGet(const string& jpath)
+{
+	deque<string> rem;
+
+	const UniValue& val = lookupPath(jpath, rem);
+	if (rem.size() != 0)
+		return NullUniValue;
+
+	return val;
+}
+
 static bool readInput()
 {
 	string rawBody;
@@ -105,6 +249,41 @@ static bool readInput()
 
 static bool processDocument()
 {
+	while (inputTokens.size() > 0) {
+		// pop next token
+		const string cmd = inputTokens.front();
+		inputTokens.pop_front();
+
+		// lookup command in command map
+		if (!cmdMap.count(cmd)) {
+			fprintf(stderr, "Unknown command %s\n", cmd.c_str());
+			return false;
+		}
+
+		// arg count validation
+		const commandInfo& cmdInfo = cmdMap[cmd];
+		if (inputTokens.size() < cmdInfo.n_args) {
+			fprintf(stderr, "Command %s missing arguments\n",
+				cmd.c_str());
+			return false;
+		}
+
+		// arg collection
+		vector<string> cmdArgs;
+		for (unsigned int i = 0; i < cmdInfo.n_args; i++) {
+			const string tok = inputTokens.front();
+			inputTokens.pop_front();
+			cmdArgs.push_back(tok);
+		}
+
+		// per-command processing
+
+		if (cmd == "get") {
+			const UniValue& val = objectGet(cmdArgs[0]);
+			jdoc = val;
+		}
+	}
+
 	return true;
 }
 
@@ -118,12 +297,19 @@ static bool writeOutput()
 
 int main (int argc, char *argv[])
 {
+	staticInit();
+
 	// parse command line
 	error_t argp_rc = argp_parse(&argp, argc, argv, 0, NULL, NULL);
 	if (argp_rc) {
 		fprintf(stderr, "%s: argp_parse failed: %s\n",
 			argv[0], strerror(argp_rc));
 		return EXIT_FAILURE;
+	}
+
+	if (doListCommands) {
+		listCommands();
+		return EXIT_SUCCESS;
 	}
 
 	if ((readStdin && !readInput()) ||
