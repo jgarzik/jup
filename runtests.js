93ee85bc53992c043548d1b9800f669fef644a3a
Jeff Garzik <jeff@bloq.com>
2019-02-02 10:46:09 -0500

    runtests: debug output more readable

--- runtests.js
+++ runtests.js
@@ -19,30 +19,41 @@ function runTest(testInfo)
 	}
 	const cmd = `cat ${dataDir}${testInfo.in} | ${jupPath}${cmdArgs}`;
 	const expectedOutput = fs.readFileSync(dataDir + testInfo.out, 'utf8');
-	console.log(cmd);
+	// console.log(cmd);
+
+	const logPrefix = `${testInfo.desc}: `;
 
 	exec(cmd, (err, stdout, stderr) => {
 		if (err) {
 			console.error(err);
+			console.error(logPrefix + 'failed');
 			process.exit(1);
 		}
 
 		if (stderr) {
 			console.error("Unexpected stderr output: " + stderr);
+			console.error(logPrefix + 'failed');
 			process.exit(1);
 		}
 		if (stdout !== expectedOutput) {
 			console.error("Unmatched stdout output");
 			console.error("EXPECTED:" + expectedOutput);
 			console.error("ACTUAL:" + stdout);
+			console.error(logPrefix + 'failed');
 			process.exit(1);
 		}
+
+		console.log(logPrefix + 'ok');
 	});
 }
 
 function runTests()
 {
 	const testList = JSON.parse(fs.readFileSync(dataDir+'all-tests.json'));
+
+	const testCount = testList.length;
+	console.log(`Running ${testCount} tests:`);
+
 	testList.forEach(function(testInfo) {
 		runTest(testInfo);
 	});
